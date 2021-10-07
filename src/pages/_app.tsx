3f71d7c001f98f7fe75ae2f36b595697ede61485
ASOCezar <aso.cezar00@gmail.com>
2021-10-07 17:57:04 -0300

    App Theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { FavoritesProvider } from "../contexts/FavoritesContext";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
+import { GlobalStyles } from "../styles/global-styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <FavoritesProvider>
-      <Component {...pageProps} />
-    </FavoritesProvider>
+    <ThemeProvider theme={theme}>
+      <FavoritesProvider>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </FavoritesProvider>
+    </ThemeProvider>
   );
 }
 export default MyApp;
